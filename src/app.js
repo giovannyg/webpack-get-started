898934577137eafb5a69ccf72f735c6b973850a8
giovanny <giovanny@messoft.com>
2020-01-17 12:09:11 -0600

    sass loader, jquery and minicss extract plugin

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import { showAlert } from './messages'
+import $ from 'jquery'
+import { v4 } from 'uuid'
+
+import "./styles.scss";
 
 document.getElementById('btn_alert')
   .addEventListener('click', showAlert);
+
+  $('#btn_jq').click(() => alert(v4()));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/app.js',
@@ -6,9 +7,30 @@ module.exports = {
     path: __dirname + '/build',
     filename: 'bundle.js'
   },
+  devServer: {
+    port: 8080
+  },
+  watchOptions: { //needed for docker npx webpack -w to detect file changes from host
+    poll: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ]
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
     })
   ]
 }
